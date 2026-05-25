2a89c01cc03310035a709b5f9db1263271de39ee
fengqi <lyf362345@gmail.com>
2026-05-25 14:56:56 +0800

    status bar

--- app/src/main/java/me/fengqi/gatusmobile/ui/screen/EndpointDetailScreen.kt
+++ app/src/main/java/me/fengqi/gatusmobile/ui/screen/EndpointDetailScreen.kt
@@ -10,6 +10,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.foundation.layout.systemBarsPadding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
@@ -70,6 +71,7 @@ fun EndpointDetailScreen(
                 color = MaterialTheme.colorScheme.onSurface,
                 fontSize = 14.sp,
                 modifier = Modifier
+                    .statusBarsPadding()
                     .clickable(onClick = onBack)
                     .padding(horizontal = 16.dp, vertical = 12.dp)
             )
